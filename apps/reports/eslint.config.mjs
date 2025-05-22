350991819dd321be12e4532a4981475fd087e313
Bulat Almukhametov <khattartmasi@rocketmail.com>
2025-05-22 16:24:06 -0300

    reports web app config changes

--- apps/reports/eslint.config.mjs
+++ apps/reports/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'hra',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'hra',
           style: 'kebab-case',
         },
       ],
